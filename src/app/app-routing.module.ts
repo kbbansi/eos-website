1801d3e09d29d0e9a7d870daf2f16e148cd11ca5
kbbansi <kwabenaampofo5@gmail.com>
2021-05-21 23:48:04 +0000

    Init website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { CheckoutCartComponent } from './components/checkout-cart/checkout-cart.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'register', component: AuthenticationComponent },
+  { path: 'login', component: AuthenticationComponent },
+  { path: 'profile', component: UserAccountComponent },
+  { path: 'view-cart', component: CheckoutCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CheckoutCartComponent } from './components/checkout-cart/checkout-cart.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    UserAccountComponent,
+    ProductDetailsComponent,
+    CheckoutCartComponent,
+    AuthenticationComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
