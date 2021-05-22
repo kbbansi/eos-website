d8bee686ed3039e31cbd2588c1a3f09218cffdc9
kbbansi <kwabenaampofo5@gmail.com>
2021-05-22 16:05:20 +0000

    Start user details pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adinkrah';
+  firstName: any;
+
+  constructor() {
+    this.firstName = sessionStorage.getItem('firstName');
+    console.log(this.firstName)
+  }
 }
--- src/app/components/user-account/user-account.component.ts
+++ src/app/components/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-account',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-account.component.css']
 })
 export class UserAccountComponent implements OnInit {
+  firstName: any;
+  lastName: any;
+  otherNames: any;
+  id: any;
+  email: any;
+  userName: any;
+  userType: any;
+  contactNo: any;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.firstName = sessionStorage.getItem('firstName');
+    this.lastName = sessionStorage.getItem('lastName');
+    this.otherNames = sessionStorage.getItem('otherNames');
+    this.id = sessionStorage.getItem('id');
+    this.email = sessionStorage.getItem('email');
+    this.userName = sessionStorage.getItem('userName');
+    this.userType = sessionStorage.getItem('userType');
+    this.contactNo = sessionStorage.getItem('contactNo');
+  }
 
   ngOnInit(): void {
+    this.loadScript()
+  }
+
+  public loadScript() {
+    let body = <HTMLDivElement> document.body;
+    let script = document.createElement('script');
+    script.innerHTML;
+    script.src = '../../../assets/sidebar.js';
+    script.async = true;
+    script.defer = true;
+    body.appendChild(script);
+  }
+
+  logout(){
+    console.log('Bye Bye %s', this.firstName);
+    sessionStorage.clear();
+    this.router.navigate(['/'])
+
   }
 
 }
